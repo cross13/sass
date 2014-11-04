d4b8a167ae1c202a218bc561d550d4f87f025559
agustin veloso <agustin.veloso@globant.com>
2014-11-04 16:46:52 -0300

    Basic checkbox functionallity implemented

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,42 @@
  */
 angular.module('sassApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    var params = {
+    	enabled: true,
+    	items: [
+	    	{
+	    		name: '3-Prong 5\' Flat Dryer Cord - Gray',
+	    		price: 29.99,
+	    		selected: true
+	    	},
+	    	{
+	    		name: '4-Prong 5\' Round Dryer Cord - Black',
+	    		price: 29.99
+	    	},
+	    	{
+	    		name: '6 ft. Snap-Lock&amp;trade; Flexible Pipe',
+	    		price: 14.99
+	    	},
+	    	{
+	    		name: 'Dryer Exhaust Close Elbow',
+	    		price: 9.99
+	    	}
+	    ],
+	    toggleSelected: function(item){
+	    	item.selected = !item.selected;
+	    },
+	    loadingEvent: 'set-available-option'
+    };
+
+    $scope.enabled = params.enabled;
+
+    $scope.items = params.items;
+
+    $scope.toggleSelected = function(item) {
+    	if ($scope.enabled) {
+	    	params.toggleSelected(item);
+	    }
+    };
+
+    $scope.loadingEvent = params.loadingEvent;
   });
